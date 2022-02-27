1d4e55741e943fced98d401117c406094202db3d
Izdhan <57706976+Prof-Iz@users.noreply.github.com>
2022-02-27 16:32:37 +0800

    change from freq to data rate

--- PWL.py
+++ PWL.py
@@ -1,5 +1,5 @@
 from engineering_notation import EngNumber
-def make_pwl(sequence:str,frequency:float,filename="pwl.txt",voltage_level = 5):
+def make_pwl(sequence:str,bit_rate:float,filename="pwl.txt",voltage_level = 5):
     """Python Function to generate digital input voltage sequence
     given bit sequence and datarate. Output waveform
     at 5V voltage level by default and saved to a
@@ -7,11 +7,11 @@ def make_pwl(sequence:str,frequency:float,filename="pwl.txt",voltage_level = 5):
 
     Args:
         sequence (str): Sequence of bits ex: "1011001"
-        frequency (float): data rate in Hz ex: 1e3 for 1Khz
+        bit_rate (float): data rate in Hz ex: 1e3 for 1 Kbps
         filename (str, optional): File to save the generated PWL. Defaults to "pwl.txt".
         voltage_level (int, optional): Peak Voltage level in PWL. Defaults to 5.
     """ 
-    bit_period = 1/frequency
+    bit_period = 1/bit_rate
     half = bit_period/2
     mid = bit_period/2
     with open(filename,"w") as f:
